19a2568301f7a2b45389f6cef05daa6208730ad1
izarshia <niktabarshia@hmail.com>
2022-12-25 14:41:14 +0330

    fix poll interaction2

--- events/interactionCreatePoll.js
+++ events/interactionCreatePoll.js
@@ -1,7 +1,8 @@
 const polls = require("../models/polls");
 const { EmbedBuilder, Collection, PermissionsBitField , ButtonBuilder, StringSelectMenuBuilder, ActionRowBuilder, TextInputBuilder  } = require('discord.js');
+const client = require('..');
 
-module.exports = async (client, interaction) => {
+client.on('interactionCreate', async interaction => {
     if (!interaction.isButton()) return;
 
     const pol = await polls.findOne({ message: interaction.message.id });
@@ -48,4 +49,4 @@ module.exports = async (client, interaction) => {
             return row;
         })
     })
-}
+})
