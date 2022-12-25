e7b5f5adb7851d68e526139d4263afaf0b838593
izarshia <niktabarshia@hmail.com>
2022-12-25 15:05:47 +0330

    fix poll interaction3335

--- events/buttonInteraction.js
+++ events/buttonInteraction.js
@@ -160,5 +160,53 @@ client.on('interactionCreate', async interaction => {
         //SendFeedBacksChannel.send(`${modalSubmitInteraction.fields.getTextInputValue('FeedbackInput')}`)
     //${modalSubmitInteraction.fields.getTextInputValue('FeedbackInput')}
       }
+
+
+      
+    const pol = await polls.findOne({ message: interaction.message.id });
+
+    if (!pol) return console.log(`not found poll`);
+
+    await interaction.deferReply({
+        ephemeral: true
+    });
+
+    if (pol.voters.includes(interaction.user.id)) return interaction.editReply({
+        embeds: [{
+            color: "15548997",
+            title: "❌ Already Voted!"
+        }]
+    });
+
+    pol.votes = pol.votes || {};
+
+    if (pol.votes[interaction.customId]) pol.votes[interaction.customId] += 1
+    else pol.votes[interaction.customId] = 1;
+
+    pol.voters.push(interaction.user.id);
+
+    await polls.findOneAndUpdate({ message: pol.message }, pol);
+
+    interaction.editReply({
+        embeds: [{
+            color: "5763719",
+            title: "✅ Voted Successfully"
+        }]
+    });
+
+    const m = interaction.message;
+
+    m.edit({
+        components: m.components.map(row => {
+            row.components = row.components?.map(v => {
+                v.emoji = v.customId
+                v.label = `${pol.votes[v.customId] || 0}`;
+
+                return v;
+            });
+
+            return row;
+        })
+    })
     
 });
