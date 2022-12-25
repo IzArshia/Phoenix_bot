5ce448c62e9641b1cecd9f01a8cb767732a3f2f6
izarshia <niktabarshia@hmail.com>
2022-12-25 15:06:38 +0330

    fix poll interaction1000

--- events/interactionCreatePoll.js
+++
@@ -1,8 +0,0 @@
-const polls = require("../models/polls");
-const { EmbedBuilder, Collection, PermissionsBitField , ButtonBuilder, StringSelectMenuBuilder, ActionRowBuilder, TextInputBuilder  } = require('discord.js');
-const client = require('..');
-
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isButton()) return console.log(`button yaft nashod !`);
-
-})
